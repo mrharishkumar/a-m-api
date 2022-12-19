01c265c7554cee9034b2bb5f7704973cb1f08bcd
harish.kumar <harish.kumar@impressico.com>
2022-12-19 17:51:55 +0530

    Use user id at place of employee id in asset requests

--- assetrequests/models.py
+++ assetrequests/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from assets.models import Asset
-from employees.models import Employee
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -13,7 +13,7 @@ class Status(models.TextChoices):
 
 class AssetRequest(models.Model):
     asset_id = models.ForeignKey(Asset, on_delete=models.CASCADE)
-    employee_id = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    employee_id = models.ForeignKey(User, on_delete=models.CASCADE)
     remarks = models.TextField(max_length=100)
     status = models.CharField(
         max_length=10, choices=Status.choices, default=Status.PENDING)
